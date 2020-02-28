90dc8aefd4c41eedd7a3279a8d431c43ce6d8850
vadimador <vadgolda@gmail.com>
2020-02-28 15:04:43 +0100

    explication pour Tom

--- app/src/main/java/com/example/projetapplimobile/MainActivity.java
+++ app/src/main/java/com/example/projetapplimobile/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.projetapplimobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = new TextView(this);
+        text = findViewById(R.id.text);
+
+        text.setText("je veux mourir.");
     }
 }
