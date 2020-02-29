fbf2b9bc2c6192f176fbd25188e0b4266d9b1682
vadimador <vadgolda@gmail.com>
2020-02-29 22:51:14 +0100

    Mise en place de l'activité de la liste des produits scannés

---
+++ app/src/main/java/com/example/projetapplimobile/ListeProduits.java
@@ -0,0 +1,14 @@
+package com.example.projetapplimobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeProduits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_produits);
+    }
+}
