2239969e47bd972a72bbc7f023a6fac79e3cbd12
vadimador <vadgolda@gmail.com>
2020-03-06 15:17:00 +0100

    ajout du calendrier pour pouvoir séléctionner sa date de péremption

--- app/src/main/java/com/example/projetapplimobile/CameraScan.java
+++ app/src/main/java/com/example/projetapplimobile/CameraScan.java
@@ -20,7 +20,7 @@ public class CameraScan extends AppCompatActivity implements ZXingScannerView.Re
     }
 
     @Override
-    public void handleResult(Result result) {
+    public void handleResult(Result result) { //c'est ce qui se passe lorsque nous avons trouvé un résultat
         ScanCodeActivity.resultat.setText(result.getText());
         onBackPressed();
     }
